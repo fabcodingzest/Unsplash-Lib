30e80611dbac382baed03d7f5221763a2169ed9e
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2022-06-08 16:32:48 +0530

    Rename some variable

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 
 const Home = () => {
-  const [photos, setPhotos] = useState([]);
+  const [phot, setPhot] = useState([]);
 
   useEffect(() => {
     fetch(
       "https://api.unsplash.com/collections/1020971/photos/?client_id=cHzzaJY0gf0Ov0CZ0qR_II4g74CCNjsIVVYviiqrB8I"
     )
-      .then((response) => {
-        response.json().then((data) => setPhotos(data));
+      .then((r) => {
+        r.json().then((d) => setPhot(d));
       })
       .catch((error) => console.error(error));
   }, []);
@@ -21,7 +21,7 @@ const Home = () => {
         <meta name="description" content="Unplash" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {photos.map(({ urls }) => (
+      {phot.map(({ urls }) => (
         <img src={urls["regular"]} />
       ))}
     </>
