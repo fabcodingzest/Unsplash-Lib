aeff3c744f6d0a6a6d4717b391036191e4904fa0
fabcodingzest <fabrizvi786@gmail.com>
2022-11-04 01:56:04 +0530

    refactored: added getServerSideProps for data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import { Container } from "@mui/material";
-import Feed from "../components/Feed";
+import Feed from "./components/Feed";
 import throttle from "lodash.throttle";
 
-const Home = () => {
+// Refactor
+const fetchImages = async (page) => {
+  const url = `${process.env.NEXT_PUBLIC_UNSPLASH_URL}/collections/${process.env.NEXT_PUBLIC_UNSPLASH_COLLECTION_ID}/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID}&page=${page}&per_page=13`;
+  const response = await fetch(url);
+  return response.json();
+};
+
+const Home = ({ initialPhotos }) => {
   const [loading, setLoading] = useState(false);
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState(initialPhotos);
   const [page, setPage] = useState(1);
   const [canFetch, setCanFetch] = useState(true);
 
   useEffect(() => {
-    fetchImages();
+    updateImages();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
-  const fetchImages = async () => {
+  const updateImages = async () => {
     setLoading(true);
-    const urlPage = `&page=${page}`;
-    const url = `${process.env.NEXT_PUBLIC_UNSPLASH_URL}/collections/${process.env.NEXT_PUBLIC_UNSPLASH_COLLECTION_ID}/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID}${urlPage}`;
     try {
-      if (canFetch) {
-        const response = await fetch(url);
-        const data = await response.json();
+      if (canFetch && page > 1) {
+        const data = await fetchImages(page);
         if (data.length === 0) {
           setCanFetch(false);
           return;
@@ -30,13 +34,9 @@ const Home = () => {
           setCanFetch(true);
         }
         setPhotos((oldData) => {
-          if (page === 1) {
-            return data;
-          } else {
-            return [...oldData, ...data];
-          }
-          setLoading(false);
+          return [...oldData, ...data];
         });
+        setLoading(false);
       }
     } catch (error) {
       setLoading(false);
@@ -65,15 +65,22 @@ const Home = () => {
   }, []);
 
   return (
-    <Container>
+    <Container maxWidth="sm">
       <Head>
-        <title>Unsplash</title>
-        <meta name="description" content="Unplash" />
+        <title>Swanky Feed</title>
+        <meta name="description" content="Swanky Feed" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Feed photos={photos} />
     </Container>
   );
 };
+
+export async function getStaticProps() {
+  const data = await fetchImages(1);
+  return {
+    props: { initialPhotos: data },
+  };
+}
 
 export default Home;
