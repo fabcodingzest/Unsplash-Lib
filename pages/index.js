e134c9743976b2f3e3130acef8839501b8ddf951
fabcodingzest <fabrizvi786@gmail.com>
2022-11-03 18:13:26 +0530

    added: dataFetching and unlimited Scroll

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,78 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import { Container } from "@mui/material";
+import Feed from "../components/Feed";
+import throttle from "lodash.throttle";
 
 const Home = () => {
-  const [phot, setPhot] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [canFetch, setCanFetch] = useState(true);
 
   useEffect(() => {
-    fetch(
-      "https://api.unsplash.com/collections/1020971/photos/?client_id=cHzzaJY0gf0Ov0CZ0qR_II4g74CCNjsIVVYviiqrB8I"
-    )
-      .then((r) => {
-        r.json().then((d) => setPhot(d));
-      })
-      .catch((error) => console.error(error));
+    fetchImages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    const urlPage = `&page=${page}`;
+    const url = `${process.env.NEXT_PUBLIC_UNSPLASH_URL}/collections/${process.env.NEXT_PUBLIC_UNSPLASH_COLLECTION_ID}/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID}${urlPage}`;
+    try {
+      if (canFetch) {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.length === 0) {
+          setCanFetch(false);
+          return;
+        } else if (data.length > 0) {
+          setCanFetch(true);
+        }
+        setPhotos((oldData) => {
+          if (page === 1) {
+            return data;
+          } else {
+            return [...oldData, ...data];
+          }
+          setLoading(false);
+        });
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const handleScroll = () => {
+    if (
+      !loading &&
+      window.innerHeight + window.scrollY >= document.body.scrollHeight - 17
+    ) {
+      setPage((oldPage) => {
+        return oldPage + 1;
+      });
+    }
+  };
+
+  useEffect(() => {
+    const event = window.addEventListener(
+      "scroll",
+      throttle(handleScroll, 500)
+    );
+    return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <>
+    <Container>
       <Head>
         <title>Unsplash</title>
         <meta name="description" content="Unplash" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {phot.map(({ urls }) => (
-        <img src={urls["regular"]} />
-      ))}
-    </>
+      <Feed photos={photos} />
+    </Container>
   );
 };
 
