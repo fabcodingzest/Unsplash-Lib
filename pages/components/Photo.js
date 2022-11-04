a5beaebb9b42785fea8718a914dd363acd2f9cec
fabcodingzest <fabrizvi786@gmail.com>
2022-11-04 23:44:37 +0530

    added: blurDataUrl

--- pages/components/Photo.js
+++ pages/components/Photo.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+const myLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+};
 function Photo({ data }) {
   const { urls, alt_description, id } = data;
   return (
@@ -11,6 +13,8 @@ function Photo({ data }) {
           height={500}
           width={500}
           objectFit="cover"
+          placeholder="blur"
+          blurDataURL="https://via.placeholder.com/150"
           src={urls.small} // Add fallback image or some check for render
           alt={alt_description}
         />
