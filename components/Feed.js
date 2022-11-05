6694cf392f51332b6be8f79c082f6dc41e6003d3
fabcodingzest <fabrizvi786@gmail.com>
2022-11-06 04:59:53 +0530

    moved: components to root folder

--- components/Feed.js
+++ components/Feed.js
@@ -7,12 +7,11 @@ function Feed({ photos }) {
   return (
     <Grid
       container
-      py={10}
       rowSpacing={{ xs: 1, sm: 2, md: 4 }}
       columnSpacing={{ xs: 1.4, sm: 2.4, md: 4.4 }}>
       {photos.map((data) => (
         <Grid item xs={4} key={data.id}>
-          <Photo data={data} />
+          <Photo data={data} width={200} height={200} />
         </Grid>
       ))}
     </Grid>
