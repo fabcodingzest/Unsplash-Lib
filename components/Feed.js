6f4b6d334bdf580b34e22bc153bea00dcd1730cb
fabcodingzest <fabrizvi786@gmail.com>
2022-11-06 08:08:08 +0530

    removed: forwardRef errors from next/link for func child comp

--- components/Feed.js
+++ components/Feed.js
@@ -6,21 +6,22 @@ import Photo from "./Photo";
 const FeedItem = ({ data }) => {
   const { id } = data;
   return (
-    <Grid item xs={4}>
-      <Link href={`/detail/${id}`} style={{ cursor: "pointer" }}>
+    <Link href={`/detail/${id}`} style={{ cursor: "pointer" }} passHref>
+      <Grid item xs={4}>
         <Photo data={data} width={200} height={200} />
-      </Link>
-    </Grid>
+      </Grid>
+    </Link>
   );
 };
 
 const Feed = ({ photos }) => {
+  console.log(photos);
   return (
     <Grid
       container
       rowSpacing={{ xs: 0, sm: 1 }}
       columnSpacing={{ xs: 0.8, sm: 1.6 }}>
-      {photos.map((data) => (
+      {photos[0].map((data) => (
         <FeedItem key={data.id} data={data} />
       ))}
     </Grid>
