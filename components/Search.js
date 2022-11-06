2deb376decbc66463242e9155d2290650ac302b0
fabcodingzest <fabrizvi786@gmail.com>
2022-11-06 06:05:00 +0530

    initiated: search component

--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Box, Stack, TextField } from "@mui/material";
+import React from "react";
 
 function Search() {
   return (
-    <div><input type="text" /></div>
-  )
+    <Stack width={{ xs: "60%", md: "70%" }} maxWidth="16rem">
+      <TextField
+        label="Search for pics..."
+        margin="none"
+        size="small"
+        fontSize="2px"
+        inputProps={{ sx: { p: { xs: 0.6, sm: 1 } } }}
+        InputLabelProps={{ sx: { fontSize: { xs: "0.8rem", sm: "1rem" } } }}
+      />
+    </Stack>
+  );
 }
 
-export default Search
+export default Search;
