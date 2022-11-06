7ca835d3758e6016e470c1c1f72a0fdab1f9d1fe
fabcodingzest <fabrizvi786@gmail.com>
2022-11-06 08:07:15 +0530

    set-up: react-query with next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { useRef } from "react";
 import Layout from "../components/Layout";
 
-const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
