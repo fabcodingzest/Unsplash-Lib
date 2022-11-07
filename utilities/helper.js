6d419b8f4f0b99c7ba60c9bbb75af83dad7de4b7
fabcodingzest <fabrizvi786@gmail.com>
2022-11-07 22:46:10 +0530

    added: helper function for detail page collection data

--- utilities/helper.js
+++ utilities/helper.js
@@ -3,3 +3,13 @@ export const getNextPageNum = (lastPage, pages) => {
   const nextPage = pages.length + 1;
   return nextPage <= totalPages ? nextPage : undefined;
 };
+
+export const getRelatedCollectionData = (imageDetails) => {
+  const relatedCollections = imageDetails.related_collections;
+  const haveRelatedCollections =
+    relatedCollections && relatedCollections.results.length > 0;
+  const firstCollectionId = relatedCollections.results[0].id;
+  return haveRelatedCollections
+    ? { haveRelatedCollections, firstCollectionId }
+    : false;
+};
