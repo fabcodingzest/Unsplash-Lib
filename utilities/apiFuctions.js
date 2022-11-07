00c570d95ac2475c365415a6c2289420841037d7
fabcodingzest <fabrizvi786@gmail.com>
2022-11-07 23:47:05 +0530

    added: export

--- utilities/apiFuctions.js
+++ utilities/apiFuctions.js
@@ -1,7 +1,7 @@
-const baseUrl = process.env.NEXT_PUBLIC_UNSPLASH_URL;
-const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID;
+export const baseUrl = process.env.NEXT_PUBLIC_UNSPLASH_URL;
+export const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID;
 export const collectionId = process.env.NEXT_PUBLIC_UNSPLASH_COLLECTION_ID;
-const perPage = 15;
+export const perPage = 15;
 
 export const fetchCollectionImages = async ({ pageParam = 1 }) => {
   try {
