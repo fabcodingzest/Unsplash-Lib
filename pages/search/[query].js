810f1e8605b6063d47daa9e385880620ddd7c63b
fabcodingzest <fabrizvi786@gmail.com>
2022-11-07 19:52:34 +0530

    refactor: search page

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -4,24 +4,9 @@ import { Box, CircularProgress, Stack, Typography } from "@mui/material";
 import { useInView } from "react-intersection-observer";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import Feed from "../../components/Feed";
-
-const PER_PAGE = 15;
-
-const fetchImages = async (pageParam, query) => {
-  try {
-    console.log(pageParam);
-    const url = `${process.env.NEXT_PUBLIC_UNSPLASH_URL}/search/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID}&query=${query}&page=${pageParam}&per_page=${PER_PAGE}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    if (data.results.length === 0) {
-      throw new Error("No Images Found");
-    }
-    return { data: data.results, total_pages: data.total_pages };
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
-};
+import { fetchSearchImages, getNextPageNum } from "../../utilities/helpers";
+import Loader from "../../components/Loader";
+import Error from "../../components/Error";
 
 function Search() {
   const { query } = useRouter().query;
@@ -39,53 +24,35 @@ function Search() {
     hasNextPage,
   } = useInfiniteQuery({
     queryKey: ["search", query],
-    queryFn: ({ pageParam = 1 }) => fetchImages(pageParam, query),
-    getNextPageParam: (lastPage, pages) => {
-      const totalPages = lastPage.total_pages;
-      const nextPage = pages.length + 1;
-      return nextPage <= totalPages ? nextPage : undefined;
-    },
+    queryFn: ({ pageParam = 1 }) => fetchSearchImages(pageParam, query),
+    getNextPageParam: getNextPageNum,
     refetchOnWindowFocus: false,
   });
 
   React.useEffect(() => {
     if (inView && hasNextPage) {
-      // setTimeout(() => , 3000);
       fetchNextPage();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inView]);
 
+  if (status === "loading" || isLoading) {
+    return <Loader />;
+  }
+
+  if (status === "error" || error) {
+    return <Error error={error} />;
+  }
+
   return (
     <Box>
-      {status === "loading" ? (
-        <Stack py={8} alignItems="center">
-          <CircularProgress />
-        </Stack>
-      ) : status === "error" ? (
-        <Stack py={2} alignItems="center">
-          <Typography fontSize="large">
-            {error.message} for{" "}
-            <Typography fontSize="large" component={"span"} fontWeight="bold">
-              `{decodeURI(query)}`
-            </Typography>
-          </Typography>
-        </Stack>
-      ) : (
-        <Box>
-          {data.pages.map((page) => {
-            console.log(page);
-            return <Feed photos={page.data} key={page.data[0].id} />;
-          })}
-          <Box ref={ref} pb={8}>
-            {(isFetching || isFetchingNextPage) && (
-              <Stack py={8} alignItems="center">
-                <CircularProgress />
-              </Stack>
-            )}
-          </Box>
-        </Box>
-      )}
+      {data.pages.map((page) => {
+        console.log(page);
+        return <Feed photos={page.data} key={page.data[0].id} />;
+      })}
+      <Box ref={ref} pb={8}>
+        {(isFetching || isFetchingNextPage) && <Loader />}
+      </Box>
     </Box>
   );
 }
