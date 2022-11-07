c6b478408f0c381c9736754961b126074313f11a
fabcodingzest <fabrizvi786@gmail.com>
2022-11-07 20:46:58 +0530

    added: detail page

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,12 +1,32 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Container } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { fetchImageDetails } from "../../utilities/apiFuctions";
+import Loader from "../../components/Loader";
+import Error from "../../components/Error";
+import DetailImage from "../../components/DetailImage";
 
 function Detail() {
   const { id } = useRouter().query;
-  return <Container maxWidth="sm">detail -{id}</Container>;
-}
+  const { data, status, error, isLoading } = useQuery({
+    queryKey: ["detail", id],
+    queryFn: () => fetchImageDetails(id),
+  });
+  console.log(data);
 
-// export
+  if (status === "loading" || isLoading) {
+    return <Loader />;
+  }
+
+  if (status === "error" || error) {
+    return <Error error={error} />;
+  }
+  return (
+    <Container maxWidth="sm" sx={{ minHeight: "100vh" }}>
+      <DetailImage data={data} />
+    </Container>
+  );
+}
 
 export default Detail;
