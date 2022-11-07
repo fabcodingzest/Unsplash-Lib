8b8e167980dcf22b822900a4b2f0028514b78585
fabcodingzest <fabrizvi786@gmail.com>
2022-11-07 22:48:20 +0530

    added: ssg with fallback block to ssr once on first req

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,32 +1,70 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Container } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import { fetchImageDetails } from "../../utilities/apiFuctions";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+import {
+  fetchCollectionImages,
+  fetchImageDetails,
+} from "../../utilities/apiFuctions";
 import Loader from "../../components/Loader";
 import Error from "../../components/Error";
 import DetailImage from "../../components/DetailImage";
+import { getRelatedCollectionData } from "../../utilities/helper";
 
-function Detail() {
+const Detail = () => {
   const { id } = useRouter().query;
   const { data, status, error, isLoading } = useQuery({
     queryKey: ["detail", id],
     queryFn: () => fetchImageDetails(id),
+    refetchOnWindowFocus: false,
   });
-  console.log(data);
 
-  if (status === "loading" || isLoading) {
+  console.log(isLoading);
+  if (isLoading) {
     return <Loader />;
   }
 
   if (status === "error" || error) {
     return <Error error={error} />;
   }
+
   return (
     <Container maxWidth="sm" sx={{ minHeight: "100vh" }}>
-      <DetailImage data={data} />
+      {status === "success" && <DetailImage data={data} />}
     </Container>
   );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export async function getStaticProps(context) {
+  const id = context.params?.id;
+  const queryClient = new QueryClient();
+
+  const imageDetails = await queryClient.fetchQuery({
+    queryKey: ["detail", id],
+    queryFn: () => fetchImageDetails(id),
+  });
+
+  const { haveRelatedCollections, firstCollectionId } =
+    getRelatedCollectionData(imageDetails);
+
+  if (haveRelatedCollections) {
+    await queryClient.prefetchInfiniteQuery({
+      queryKey: ["collection", firstCollectionId],
+      queryFn: fetchCollectionImages,
+    });
+  }
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+    },
+  };
 }
 
 export default Detail;
