cf6f75b42dc5f2b094262f0080d9cacfd3e81f37
fabcodingzest <fabrizvi786@gmail.com>
2022-11-08 18:11:00 +0530

    extracted: scroll logic in custom hook

---
+++ hooks/useScroll.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+export default function useScroll(fetchNextPage, hasNextPage) {
+  const { ref, inView } = useInView();
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
+  return ref;
+}
