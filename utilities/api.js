4a3e0eb9c1b6e55d6fe2a0844b53bc91075eafde
fabcodingzest <fabrizvi786@gmail.com>
2022-11-08 18:10:23 +0530

    added: bearer access token

--- utilities/api.js
+++ utilities/api.js
@@ -3,12 +3,14 @@ import axios from "axios";
 export const baseUrl = process.env.NEXT_PUBLIC_UNSPLASH_URL;
 export const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID;
 export const collectionId = process.env.NEXT_PUBLIC_UNSPLASH_COLLECTION_ID;
-export const perPage = 15;
+export const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 export default axios.create({
   baseURL: baseUrl,
   params: {
     client_id: clientId,
-    per_page: perPage,
+  },
+  headers: {
+    Authorization: "Bearer " + token,
   },
 });
